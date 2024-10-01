fbbe0516c4e73367ef7441314c4acbf8f221caaf
Jayson Rhynas <jayrhynas@gmail.com>
2024-10-01 08:58:39 -0400

    Added usage examples to main.swift

--- Sources/RawRepresentableEnumClient/main.swift
+++ Sources/RawRepresentableEnumClient/main.swift
@@ -9,3 +9,28 @@ enum Foo {
     @DefaultCase
     case unknown(Int)
 }
+
+print(Foo(rawValue: 1))        // a
+print(Foo(rawValue: 2))        // b
+print(Foo(rawValue: 3))        // unknown(3)
+
+print(Foo.a.rawValue)          // 1
+print(Foo.b.rawValue)          // 2
+print(Foo.unknown(3).rawValue) // 3
+
+// raw values are automatic for Strings
+@RawRepresentable<String>
+enum Bar {
+    case a
+    case b
+    @DefaultCase
+    case unknown(String)
+}
+
+print(Bar(rawValue: "a"))        // a
+print(Bar(rawValue: "b"))        // b
+print(Bar(rawValue: "c"))        // unknown(c)
+
+print(Bar.a.rawValue)            // a
+print(Bar.b.rawValue)            // b
+print(Bar.unknown("c").rawValue) // c
